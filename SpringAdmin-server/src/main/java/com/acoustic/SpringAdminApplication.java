b3dd851e87a26fde59c81c09dbf979818fca6cb9
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-08-05 19:34:34 +0200

    project finished

--- SpringAdmin-server/src/main/java/com/acoustic/SpringAdminApplication.java
+++ SpringAdmin-server/src/main/java/com/acoustic/SpringAdminApplication.java
@@ -5,10 +5,12 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableEurekaClient
+@EnableScheduling
 public class SpringAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringAdminApplication.class, args);
